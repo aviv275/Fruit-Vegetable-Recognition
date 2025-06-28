912d1eefde106de4a3d89366209d1243f45aba16
Aviv274 <aviv275@student.ie.edu>
2025-06-28 16:30:44 +0200

    cloud

--- App.py
+++ App.py
@@ -229,6 +229,7 @@ def run():
     if img_file is not None:
         img = Image.open(img_file).resize((250, 250))
         st.image(img, use_container_width=False)
+        os.makedirs('./upload_images', exist_ok=True)
         save_image_path = './upload_images/' + img_file.name
         with open(save_image_path, "wb") as f:
             f.write(img_file.getbuffer())
