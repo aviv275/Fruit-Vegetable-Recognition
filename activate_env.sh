2f73c359fc1505f5e168881d9a1a227c02da70fb
Aviv274 <aviv275@student.ie.edu>
2025-06-28 14:31:49 +0200

    Initial commit: Fruit and Vegetable Classification App

---
+++ activate_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script to activate the virtual environment
+echo "Activating virtual environment..."
+source .env/bin/activate
+echo "Virtual environment activated!"
+echo "You can now run your Python scripts."
+echo ""
+echo "To deactivate, run: deactivate" 
